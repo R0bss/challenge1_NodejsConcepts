415c1a891fe8e135ff350108b673646ee1431844
Roberto Luiz <luizroberto1957@hotmail.com>
2022-09-20 20:59:37 -0300

    Create Post Users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers;
+
+  const user = users.find((customer) => user.username === username);
+
+  if(!user) {
+    return response.status(400).json({error: 'Not Found'});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 app.post('/users', (request, response) => {
   // Complete aqui
+  const {name, username} = request.body;
+
+  const checksExistsUserAccount = users.some(
+    (user) => user.username === username
+  );
+
+  if (checksExistsUserAccount) {
+    return response.status(400).json({success: 'User already exists!'});
+  }
+
+  users.push({	
+    username,
+    name,
+    id: uuidv4(),
+    todos: []
+  });
+
+  return response.status(201).send();
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
